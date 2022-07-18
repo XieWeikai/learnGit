2df0ea7b369b12ada55943613f9292cb07a58a2a
XieWeikai <1263212259@qq.com>
2022-07-18 23:10:48 +0800

    update a.c

--- a.c
+++ a.c
@@ -9,5 +9,10 @@ int main(){
 
 	printf("hello,wql!\n");
 
+	int a,b;
+	printf("enter two number:");
+	scanf("%d%d",&a,&b);
+	int add(int,int);
+	printf("a + b = %d\n",add(a,b)); // test function add
 	return 0;
 }
