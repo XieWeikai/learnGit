37ae09602611f5b4cd4574444a1f5c348cad0381
CSW <wcs6190@163.com>
2022-07-20 17:51:37 +0800

    wcs's first try

--- a.c
+++ a.c
@@ -3,11 +3,11 @@
 int main(){
 	printf("hello,wcs!!!!!!!!!!!!\n");  // 注意我在test分支改了这一行
 
-
+	
 	printf("hello,xwk!????????????????\n"); // 在master分支改了这一行
 	printf("hello,xwk!!!!!!!!!!!!!\n"); // 在新的test分支修改了这一行
-
-
+	printf("Bobo bro, daisuki dayo!!!!!\n") 
+	
 	printf("hello,dogdog!\n")
 
 	printf("hello,wql!!!!!!!!!!!!!!!\n"); // 注意我在master分支改了这一行
